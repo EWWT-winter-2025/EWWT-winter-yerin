ca0c95364a32dcd605384e73cf5d0a154c25345c
gimye <gimyedev@gmail.com>
2025-01-18 23:13:13 +0900

    Design : Signup.js 디자인 완료료

--- ewwt-winter-yerin/src/pages/signup/Signup.js
+++ ewwt-winter-yerin/src/pages/signup/Signup.js
@@ -1,9 +1,32 @@
+import styles from "../signup/Signup.module.css";
+import logo from '../../assets/logo.png'
+
 const Signup = () => {
     return (
-        <div>
-            <h1>Signup</h1>
-            {/* Add your signup form here */}
-        </div>
+        <div className={styles.signuppage}>
+                    <div className={styles.signup_container}>
+                        <div className={styles.logo_container}>
+                           <img src={logo} alt="Company Logo" className="login-logo" />
+                        </div>
+                        <hr className={styles.divider}/>
+                        <div className={styles.input_container}>
+                            <div className={styles.input}>
+                                <input type="text" placeholder="id를 입력해주세요" />
+                            </div>
+                            <div className={styles.input}>
+                                <input type="text" placeholder="pw를 입력해주세요" />
+                            </div>
+                            <div className={styles.input}>
+                                <input type="text" placeholder="pw를 입력해주세요" />
+                            </div>
+                            <div className={styles.input}>
+                                <input type="text" placeholder="별명을 입력해주세요" />
+                            </div>
+                            <div className={styles.submit}><p>회원가입</p></div>
+                        </div>
+        
+                    </div>
+                </div>
     );
 }
 

d3c1bce5d810a82c0739138ec85e58b129bfa470
gimye <gimyedev@gmail.com>
2025-01-19 00:09:19 +0900

    Design : Home.js 디자인 완료료

--- ewwt-winter-yerin/src/pages/home/Home.js
+++ ewwt-winter-yerin/src/pages/home/Home.js
@@ -1,7 +1,31 @@
+import styles from "../home/Home.module.css";
+import logo from '../../assets/logo.png'
+
 const Home = () => {
     return (
-        <div>
-            <h1>Home Page</h1>
+        <div className={styles.homepage}>
+            <div className={styles.container_box}>
+                <div className={styles.left_container}>
+                    <div className={styles.logo_container}>
+                       <img src={logo} alt="Company Logo" className="login-logo" />
+                    </div>
+                    <div className={styles.nickname_container}>
+                       <p>nickname님의 공간</p>
+                    </div>
+                    <div className={styles.logout_btn}>
+                    <p>로그아웃</p>
+                </div>
+                </div>
+            <div className={styles.right_container}>
+                <div className={styles.text_btn}>
+                    <p>회원탈퇴</p>
+                </div>
+                <div className={styles.group_container}>
+
+                </div>
+            </div>
+            </div>
+            
         </div>
     )
 }
